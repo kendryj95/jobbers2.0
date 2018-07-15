7d354c47131585d26bc904798540a427d2d5e742
Ing. Kendry Ortiz <ortizkendry95@gmail.com>
2018-07-15 16:17:07 -0400

    Paginación de módulo de listado de empresas

--- local/resources/views/empresas_ver.php
+++ local/resources/views/empresas_ver.php
@@ -100,7 +100,7 @@
                 </div>
                 <!-- Tags Bar -->
                 <div class="filterbar">
-                  <h5><?php echo count($empresas);?> Empresas</h5>
+                  <h5><?php echo $totalEmpresas;?> Empresas</h5>
                 </div>
               </div>
               <div class="emply-list-sec">
@@ -121,17 +121,43 @@
                 </div><!-- Employe List -->
                 <?php endforeach; ?>
                         
-                              <!-- <div class="pagination">
-                                <ul>
-                                  <li class="prev"><a href=""><i class="la la-long-arrow-left"></i> Prev</a></li>
-                                  <li><a href="">1</a></li>
-                                  <li class="active"><a href="">2</a></li>
-                                  <li><a href="">3</a></li>
-                                  <li><span class="delimeter">...</span></li>
-                                  <li><a href="">14</a></li>
-                                  <li class="next"><a href="">Next <i class="la la-long-arrow-right"></i></a></li>
-                                </ul>
-                                </div> --><!-- Pagination -->
+                                <div class="pagination">
+                                  <ul>
+                                    <?php  
+                                      if (isset($_GET['pag'])) {
+                                        if ($_GET['pag'] != 1) {
+                                          $previous = intval($_GET['pag']) - 1;
+                                        } else {
+                                          $previous = 1;
+                                        }
+                                      } else {
+                                        $previous = 1;
+                                      }
+                                    ?>
+                                    <li class="prev"><a href="empresas?pag=<?= $previous ?>"><i class="la la-long-arrow-left"></i> Prev</a></li>
+                                    <!-- <li><a href="">1</a></li>
+                                    <li class="active"><a href="">2</a></li>
+                                    <li><a href="">3</a></li>
+                                    <li><span class="delimeter">...</span></li>
+                                    <li><a href="">14</a></li> -->
+                                    <?php for ($i=0;$i<$paginas;$i++): ?>
+                                      <?php $active = isset($_GET['pag']) ? ($i+1) == $_GET['pag'] ? 'active' : "" : ($i+1) == 1 ? 'active' : "" ?>
+                                      <li class="<?= $active ?>"><a href="empresas?pag=<?= $i+1 ?>"><?= $i+1 ?></a></li>
+                                    <?php endfor; ?>
+                                    <?php  
+                                      if (isset($_GET['pag'])) {
+                                        if ($_GET['pag'] != $paginas) {
+                                          $next = intval($_GET['pag']) + 1;
+                                        } else {
+                                          $next = $paginas;
+                                        }
+                                      } else {
+                                        $next = 2;
+                                      }
+                                    ?>
+                                    <li class="next"><a href="empresas?pag=<?= $next ?>">Next <i class="la la-long-arrow-right"></i></a></li>
+                                  </ul>
+                                </div> <!-- Pagination -->
                               </div>
                             </div>
                           </div>
