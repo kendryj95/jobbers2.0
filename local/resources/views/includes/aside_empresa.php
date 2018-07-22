f719096dae41c3bb531783baf4d3e7844c13eae2
Ing. Kendry Ortiz <ortizkendry95@gmail.com>
2018-07-21 23:04:28 -0400

    Colocar elmismo menu en el mismo ordenq ue esta en el aside de empresa en el menu dell header & Ver el aside de empresas dejar mayuscula solo la primera letra de cada frase

--- local/resources/views/includes/aside_empresa.php
+++ local/resources/views/includes/aside_empresa.php
@@ -5,21 +5,21 @@
 				<li class="inner-child">
 					<a href="#" title="Mi empresa"><i class="la la-file-text"></i>Mi empresa</a>
 					<ul>
-						<li onclick="location.href='detalle?e=<?= session()->get("emp_ide") ?>'"><a href="detalle?e=<?= session()->get("emp_ide") ?>" title="Mi Perfil">Mi Perfil</a></li>
-						<li onclick="location.href='perfil?e=<?= session()->get("emp_ide") ?>'"><a href="perfil?e=<?= session()->get("emp_ide") ?>" title="Editar Perfil">Editar Perfil</a></li>
+						<li onclick="location.href='detalle?e=<?= session()->get("emp_ide") ?>'"><a href="detalle?e=<?= session()->get("emp_ide") ?>" title="Mi Perfil">Mi perfil</a></li>
+						<li onclick="location.href='perfil?e=<?= session()->get("emp_ide") ?>'"><a href="perfil?e=<?= session()->get("emp_ide") ?>" title="Editar Perfil">Editar perfil</a></li>
 					</ul>
 				</li>
 				<li class="inner-child">
 					<a href="#" title="Ofertas de Trabajo"><i class="la la-briefcase"></i>Ofertas de Trabajo</a>
 					<ul>
-						<li onclick="location.href='ofertas'"><a href="ofertas" title="Ver Ofertas de Trab.">Ver Ofertas de Trab.</a></li>
-						<li onclick="location.href='new_post'"><a href="new_post" title="Nueva Oferta de Trab.">Nueva Oferta de Trab.</a></li>
+						<li onclick="location.href='ofertas'"><a href="ofertas" title="Ver Ofertas de Trab.">Ver ofertas</a></li>
+						<li onclick="location.href='new_post'"><a href="new_post" title="Nueva Oferta de Trab.">Nueva oferta</a></li>
 					</ul>
 				</li>
 				<li class="inner-child">
 					<a href="#" title="Planes"><i class="la la-trophy"></i>Planes</a>
 					<ul>
-						<li onclick="location.href='planes'"><a href="planes" title="Manejar Planes">Manejar Planes</a></li>
+						<li onclick="location.href='planes'"><a href="planes" title="Manejar Planes">Manejar planes</a></li>
 					</ul>
 				</li>
 				<li onclick="location.href='../logout'"><a href="../logout" title=""><i class="la la-unlink"></i>Salir</a></li>
