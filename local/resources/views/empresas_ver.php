0cfc00fb2c37a3c3c3941d9bd99fd4312b5ffc27
Miguel <miguelmendozafiguera@gmail.com>
2018-08-27 01:27:33 -0400

    Pantalla de empresas igual a la de ofertas (a la espera de la infromacion de las empresas). Fix menores en visualizacion de los filtros en ofertas y empresas. Fix altura del carousel

--- local/resources/views/empresas_ver.php
+++ local/resources/views/empresas_ver.php
@@ -50,18 +50,17 @@
       </div>
     </section>
     <section>
-      <div class="block no-padding">
+      <div class="block no-padding back-offers">
         <div class="container">
           <div class="row no-gape">
           <div class="btn-showfilter">
             <button class="btn btn-primary" id="showFilters">Mostrar filtros <i class="fa fa-filter"></i></button>
           </div>
-            <aside class="col-lg-3 column border-right" id="side-offers" style="padding-left: 0px;
-              ">
+            <aside class="col-lg-3 column border-right" id="side-offers" style="padding-left: 0px; border-color: transparent">
               
               <form action="empresas" method="POST" id="form_filter">
                 <input type="hidden" name="_token" value="<?php echo csrf_token();?>">
-                <div class="widget">
+                <div class="widget filter-offer">
                   <h3 class="sb-title open">Cargo</h3>
                   <div class="search_widget_job">
                     <div class="field_w_search">
@@ -71,31 +70,32 @@
                     </div>
                   </div>
                 </div>
-                <div class="widget">
+                <div class="widget filter-offer">
                   <h3 class="sb-title open">Industrias</h3>
-                  <div class="specialism_widget" style="">
+                  <div class="specialism_widget">
                     <div class="simple-checkbox">
                       <?php foreach ($sectores as $sect): ?>
-                      <p><input type="checkbox" onclick="filter()" name="sectores[]" value="<?= $sect->id_sector ?>" id="sect_<?= $sect->id_sector ?>"><label for="sect_<?= $sect->id_sector ?>" id="label_sect_<?= $sect->id_sector ?>"><?= $sect->sector ?> </label>&nbsp;(<?= $sect->cantidad ?>)</p>
+                      <p><input type="checkbox" onclick="filter()" name="sectores[]" value="<?= $sect->id_sector ?>" id="sect_<?= $sect->id_sector ?>"><label for="sect_<?= $sect->id_sector ?>" id="label_sect_<?= $sect->id_sector ?>"><?= $sect->sector ?> (<?= $sect->cantidad ?>)</label></p>
                       <?php endforeach; ?>
-                  </div>
-                </div>
-                <div class="widget">
-                  <h3 class="sb-title open">Provincia</h3>
-                  <div class="specialism_widget" style="">
-                    <div class="simple-checkbox">
-                      <?php foreach ($provincias as $prov): ?>
-                      <p><input type="checkbox" onclick="filter()" name="provincias[]" value="<?= $prov->id_provincia ?>" id="prov_<?= $prov->id_provincia ?>"><label for="prov_<?= $prov->id_provincia ?>" id="label_prov_<?= $prov->id_provincia ?>"><?= $prov->provincia ?></label>&nbsp; (<?= $prov->cantidad ?>)</p>
-                        <?php endforeach; ?>
                     </div>
                   </div>
                 </div>
-                <div class="widget">
+                <div class="widget filter-offer">
+                  <h3 class="sb-title open">Provincia</h3>
+                  <div class="specialism_widget">
+                    <div class="simple-checkbox">
+                      <?php foreach ($provincias as $prov): ?>
+                      <p><input type="checkbox" onclick="filter()" name="provincias[]" value="<?= $prov->id_provincia ?>" id="prov_<?= $prov->id_provincia ?>"><label for="prov_<?= $prov->id_provincia ?>" id="label_prov_<?= $prov->id_provincia ?>"><?= $prov->provincia ?> (<?= $prov->cantidad ?>)</label></p>
+                      <?php endforeach; ?>
+                    </div>
+                  </div>
+                </div>
+                <div class="widget filter-offer">
                   <h3 class="sb-title open">Localidad</h3>
-                  <div class="specialism_widget" style="">
+                  <div class="specialism_widget">
                     <div class="simple-checkbox">
                       <?php foreach ($localidades as $loc): ?>
-                      <p><input type="checkbox" onclick="filter()" name="localidades[]" value="<?= $loc->id_localidad ?>" id="loc_<?= $loc->id_localidad ?>"><label for="loc_<?= $loc->id_localidad ?>" id="label_loc_<?= $loc->id_localidad ?>"><?= $loc->localidad ?></label>&nbsp; (<?= $loc->cantidad ?>)</p>
+                      <p><input type="checkbox" onclick="filter()" name="localidades[]" value="<?= $loc->id_localidad ?>" id="loc_<?= $loc->id_localidad ?>"><label for="loc_<?= $loc->id_localidad ?>" id="label_loc_<?= $loc->id_localidad ?>"><?= $loc->localidad ?> (<?= $loc->cantidad ?>)</label></p>
                       <?php endforeach; ?>
                     </div>
                   </div>
@@ -114,6 +114,52 @@
                 <div class="filterbar">
                   <h5><?php echo $totalEmpresas;?> Empresas</h5>
                 </div>
+              </div>
+              <div class="job-listings-sec">
+                <div class="job-listing wtabs">
+										<div class="mobile">
+											<img src="http://urbancomunicacion.com/wp-content/uploads/2017/08/Historia-del-logotipo-de-Coca-Cola-Urban-comunicacion.png" class="img-fluid img-oferta" alt="">
+											<p class="nombre-img">Coca Cola</p>
+										</div>
+										<div class="job-title-sec container-desc-oferta">
+											<h5 class="title-recom">Titulo de la Empresa <a href="#"><span style="float: right; color: #bbbbbb; font-size: 15px; font-weight: 400;"><sup>Denunciar</sup> <i class="fa fa-exclamation-circle exclamation-icon"></i></span></a></h5>
+											<p class="time-pub"><i class="fa fa-calendar"></i> Publicada Hoy a las 10:58 Am - Termina: 08/12/2018</p>
+											<p class="desc-oferta">Mauris pulvinar efficitur quam nec consequat. Vestibulum eu luctus eros. Praesent non erat ullamcorper, ultrices tellus sed, egestas massa. Nunc mollis ipsum non nunc aliquet blandit. Praesent ullamcorper, libero id maximus mollis, leo neque hendrerit ligula, a egestas augue ipsum at lectus. Donec a ligula porta, vulputate mauris quis, sodales elit. </p>
+											<br>
+											<div class="job-lctn">
+												Cocacola&nbsp;
+												<i class="fa fa-star gold"></i>
+												<i class="fa fa-star gold"></i>
+												<i class="fa fa-star gold"></i>
+												<i class="fa fa-star gold"></i>
+												<i class="fa fa-star gold"></i>
+												&nbsp;
+												<i class="fa fa-eye"></i>11&nbsp;
+												<i class="fa fa-heart red"></i>3&nbsp;
+												<i class="fa fa-clock-o mr-0"></i>
+												<span class="disponibilidad">FullTime</span>&nbsp;
+												<i class="fa fa-wheelchair blue"></i>
+												<div class="desk" style="float: right">
+													<a href="#"><span class="container-fb"><i class="fa fa-facebook mr-0"></i></span></a>
+													<a href="#"><span class="container-in"><i class="fa fa-linkedin mr-0"></i></span></a>
+													<a href="#"><span class="container-tw"><i class="fa fa-twitter mr-0"></i></span></a>
+													<a href="#"><span class="container-ig"><i class="fa fa-instagram mr-0"></i></span></a>
+													<a href="#"><span class="container-web"><i class="fa fa-globe mr-0"></i></span></a>
+												</div>
+												<p class="container-media mobile" style="margin-bottom: 0;">
+													<a href="#"><span class="container-fb" style="float: inherit"><i class="fa fa-facebook" style="margin-left: 4px; vertical-align: text-top"></i></span></a>
+													<a href="#"><span class="container-in" style="float: inherit"><i class="fa fa-linkedin mr-0" style="padding:4px; margin-left: 0px; font-size: 13px; vertical-align: super;"></i></span></a>
+													<a href="#"><span class="container-tw" style="float: inherit"><i class="fa fa-twitter mr-0" style="padding:3px; margin-left: 0px; vertical-align: super; font-size: 14px;"></i></span></a>
+													<a href="#"><span class="container-ig" style="float: inherit"><i class="fa fa-instagram mr-0" style="padding:3px; margin-left: 0px; vertical-align: super; font-size: 14px;"></i></span></a>
+													<a href="#"><span class="container-web" style="float: inherit"><i class="fa fa-globe mr-0" style="padding:3px; margin-left: 0px; vertical-align: super; font-size: 14px;"></i></span></a>
+												</p>
+											</div>
+										</div>
+										<div class="job-style-bx container-img-oferta-i desk">
+											<img src="http://urbancomunicacion.com/wp-content/uploads/2017/08/Historia-del-logotipo-de-Coca-Cola-Urban-comunicacion.png" class="img-fluid img-oferta" alt="">
+											<p class="nombre-img">Coca Cola</p>
+										</div>
+									</div>
               </div>
               <div class="emply-list-sec">
                 <?php foreach ($empresas as $empresa): ?>
