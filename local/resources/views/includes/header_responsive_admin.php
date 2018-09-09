b7ee1008631c1aede0418183ad51891285f7dac7
Ing. Kendry Ortiz <ortizkendry95@gmail.com>
2018-09-09 15:42:11 -0500

    Fix a pequeño bug

--- local/resources/views/includes/header_responsive_admin.php
+++ local/resources/views/includes/header_responsive_admin.php
@@ -10,56 +10,20 @@ $back="../administrator_candidatos_ver.php";
 	<div class="responsive-menubar">
 		<div class="res-logo" style="background-color: #fff;padding-left: 25px;padding-right: 25px; border-radius: 10px;"><a href="inicio" title=""><img src="https://www.jobbersargentina.net/img/logo_d.png" alt="Logo Jobbers" /></a></div>		<div class="menu-resaction">
 			<div class="res-openmenu">
-			<!-- TODO: REVISAR UNA MEJOR MANERA DE HACER ESTO SI LA HAY.
-						VALIDAR QUE EDIT LUEGO DEL SLASH LLEVA UN ID  -->
-				<?php $url = basename($_SERVER["REQUEST_URI"]); 
-				if($url == 'plantillas' || 
-				$url == 'ofertas-renovar' || 
-				$url == 'cursos-aprobar' ||
-				$url == 'nuevo' ||
-				$url == 'create' ||
-				$url == 'edit/' ||
-				$url == 'publicar' ||
-				$url == 'categorias' ||
-				is_numeric($url)): ?>
-					<img src="../../local/resources/views/images/icon.png" alt="" /> Menú
-				<?php else: ?>
-					<img src="../local/resources/views/images/icon.png" alt="" /> Menú
-				<?php endif;?>
-
+			
+				<img src="<?= asset('local/resources/views/images/icon.png') ?>" alt="Menú" /> Menú
+				
 			</div>
 			<div class="res-closemenu">
-			<!-- TODO: REVISAR UNA MEJOR MANERA DE HACER ESTO SI LA HAY.
-						VALIDAR QUE EDIT LUEGO DEL SLASH LLEVA UN ID  -->
-				<?php $url = "".basename($_SERVER["REQUEST_URI"]); 
-				if($url == 'plantillas' || 
-				$url == 'ofertas-renovar' || 
-				$url == 'cursos-aprobar' ||
-				$url == 'nuevo' ||
-				$url == 'create' ||
-				$url == 'edit/' ||
-				$url == 'publicar' ||
-				$url == 'categorias' ||
-				is_numeric($url)): ?>
-					<img src="../../local/resources/views/images/icon2.png" alt="" /> Cerrar
-				<?php else: ?>
-					<img src="../local/resources/views/images/icon2.png" alt="" /> Cerrar
-				<?php endif;?>
+			
+				
+				<img src="<?= asset('local/resources/views/images/icon2.png') ?>" alt="Cerrar" /> Cerrar
 
 			</div>
 		</div>
 	</div>
 	<div class="responsive-opensec">
-		<!-- <div class="btn-extars">
-			<?php if (session()->get('tipo_usuario')==2 || session()->get('tipo_usuario')== 1): ?>
-				<?php if (session()->get('tipo_usuario')==2): ?>
-					<a href="<?= $back ?>candidashboard" title="" class="my-panel">Mi panel</a>
-				<?php elseif (session()->get('tipo_usuario')==1): ?>
-					<a href="#" title="" class="my-panel">Mi panel</a>
-				<?php endif; ?>
-			<?php endif; ?>
-			 </div> -->
-			
+		
 			<div class="responsivemenu res-menu-cand" style="margin-top: 10px;">
 				<div class="widget">
 					<div class="tree_widget-sec">
